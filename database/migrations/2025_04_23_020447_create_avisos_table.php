e416ea8cd03b388effb29ebdda243eeaaf8658e3
fabricioaquino <fabricio_rbfc@hotmail.com>
2025-04-22 23:31:08 -0300

    primeiro commt

---
+++ database/migrations/2025_04_23_020447_create_avisos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('avisos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 80);
+            $table->timestamp('data_publicacao');
+            $table->date('data_expedicao');
+            $table->enum('status')
+            $table->enum('obrigatorio')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('avisos');
+    }
+};
